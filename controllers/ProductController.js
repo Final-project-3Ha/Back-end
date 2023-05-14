596d82d260b3accbeeed529921ea5129c7701166
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-05-14 23:12:56 +0300

    Sort By...

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -7,9 +7,20 @@ const getProducts = async (req, res, next) => {
   try {
     const pageNum =Number(req.query.pageNum) || 1
     const totalProducts = await Product.countDocuments({})
+
+// Sort Product By name, price etc...
+
+let sort = {}
+const sortOption = req.query.sort || ""
+if (sortOption) {
+    let sortOpt = sortOption.split("_")
+    sort = { [sortOpt[0]]: Number(sortOpt[1]) }
+    // console.log(sort);
+}
+
     const products = await Product.find({})
       .skip(recordsPerPage * (pageNum - 1))
-      .sort({ name: 1 })
+      .sort(sort)
       .limit(recordsPerPage);
     res.json({ products, pageNum, paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage) 
     });
