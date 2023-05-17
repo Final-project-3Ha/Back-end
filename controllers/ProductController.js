3451520f6517c75a820e65b9c26821ce423d2693
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-05-17 11:57:00 +0300

    finished filtering&& GetById

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,32 +1,140 @@
 import Product from "../models/ProductModel.js";
 import recordsPerPage from "../config/pagination.js";
+import { request } from "express";
 
 // Get All The Products
 
 const getProducts = async (req, res, next) => {
   try {
-    const pageNum =Number(req.query.pageNum) || 1
-    const totalProducts = await Product.countDocuments({})
+    // Filtering By price
 
-// Sort Product By name, price etc...
+    let query = {};
+    let queryCondition = false;
 
-let sort = {}
-const sortOption = req.query.sort || ""
-if (sortOption) {
-    let sortOpt = sortOption.split("_")
-    sort = { [sortOpt[0]]: Number(sortOpt[1]) }
-    // console.log(sort);
-}
+    let priceQueryCondition = {};
+    if (req.query.price) {
+      queryCondition = true;
+      priceQueryCondition = { price: { $lte: Number(req.query.price) } };
+    }
 
-    const products = await Product.find({})
+    // Filtering By Rating
+
+    let ratingQueryCondition = {};
+    if (req.query.rating) {
+      queryCondition = true;
+      ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
+    }
+
+    // Search for a specific Category
+
+    let categoryQueryCondition = {};
+    const categoryName = req.params.categoryName || "";
+
+    // get specific category using search bar
+
+    if (categoryName) {
+      queryCondition = true;
+      let a = categoryName.replaceAll(",", "/");
+      var regEx = new RegExp("^" + a);
+      categoryQueryCondition = { category: regEx };
+    }
+
+    // get specific category using filtering page
+
+    if (req.query.category) {
+      queryCondition = true;
+      let a = req.query.category.split(",").map((item) => {
+        if (item) return new RegExp("^" + item);
+      });
+      categoryQueryCondition = {
+        category: { $in: a },
+      };
+    }
+
+    let attrsQueryCondition = [];
+    if (req.query.attrs) {
+      attrsQueryCondition = req.query.attrs.split(",").reduce((acc, item) => {
+        if (item) {
+          let a = item.split("-");
+          let values = [...a];
+          values.shift(); // remove first item
+          let a1 = {
+            attrs: { $elemMatch: { key: a[0], value: { $in: values } } },
+          };
+          acc.push(a1);
+          // console.dir(acc, {depth: null})
+          return acc;
+        } else return acc;
+      }, []);
+      // console.dir(attrsQueryCondition, {depth: null})
+      queryCondition = true;
+    }
+
+    // Pagination Methods
+
+    const pageNum = Number(req.query.pageNum) || 1;
+
+    // Sort Product By name, price etc...
+
+    let sort = {};
+    const sortOption = req.query.sort || "";
+    if (sortOption) {
+      let sortOpt = sortOption.split("_");
+      sort = { [sortOpt[0]]: Number(sortOpt[1]) };
+      
+    }
+
+    // sort products through search Box
+
+    const searchQuery = req.params.searchQuery || "";
+    let searchQueryCondition = {};
+    let select = {};
+    if (searchQuery) {
+      queryCondition = true;
+      searchQueryCondition = { $text: { $search: searchQuery } };
+      select = {
+        score: { $meta: "textScore" },
+      };
+      sort = { score: { $meta: "textScore" } };
+    }
+
+    if (queryCondition) {
+      query = {
+        $and: [
+          priceQueryCondition,
+          ratingQueryCondition,
+          categoryQueryCondition,
+          searchQueryCondition,
+          ...attrsQueryCondition,
+        ],
+      };
+    }
+
+    const totalProducts = await Product.countDocuments(query);
+    const products = await Product.find(query)
+      .select(select)
       .skip(recordsPerPage * (pageNum - 1))
       .sort(sort)
       .limit(recordsPerPage);
-    res.json({ products, pageNum, paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage) 
+    res.json({
+      products,
+      pageNum,
+      paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage),
     });
   } catch (err) {
     next(err);
   }
 };
 
-export default { getProducts };
+// Get Product By ID
+
+const getProductById = async(req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id).populate ("reviews").orFail()
+    res.json(product);
+  } catch (err) {
+    next(err)
+  }
+}
+
+export default { getProducts, getProductById };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,12 @@ const router = express.Router();
 import productController from "../controllers/ProductController.js";
 
 router.get("/", productController.getProducts);
+router.get("/category/:categoryName", productController.getProducts);
+router.get("/category/:categoryName/search/:searchQuery", productController.getProducts);
+router.get("/search/:searchQuery", productController.getProducts);
+router.get("/:id", productController.getProductById);
+
+
 // router.post("/", productController.newCategory)
 // router.delete("/:category", productController.deleteCategory);
 // router.post("/attr", productController.saveAttr);
