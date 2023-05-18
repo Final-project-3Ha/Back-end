ad2fceb5622e847a8b4e46d4d7e83976033ddfab
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-05-19 01:58:28 +0300

    Create Cookies

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/UserModel.js";
 import hashPassword from "../utils/hashPassword.js";
+import generateAuthToken from "../utils/generateAuthToken.js";
 
 // get all users
 
@@ -23,16 +24,42 @@ const registerUser = async (req, res, next) => {
 
     const userExists = await User.findOne({ email });
     if (userExists) {
-      return res.status(400).json({ error: "User already exists" });
+      return res.status(400).send("User already exists");
     } else {
       const hashedPassword = hashPassword(password);
       const user = await User.create({
         name,
         lastName,
         email: email.toLowerCase(),
-        password: hashedPassword
-      })
-      return res.status(200).send(user)
+        password: hashedPassword,
+      });
+      return res
+        .cookie(
+          "access_token",
+          generateAuthToken(
+            user._id,
+            user.name,
+            user.lastName,
+            user.email,
+            user.isAdmin
+          ),
+          {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+          }
+        )
+        .status(201)
+        .json({
+          success: "User created successfully",
+          userCreated: {
+            _id: user._id,
+            name: user.name,
+            lastName: user.lastName,
+            email: user.email,
+            isAdmin: user.isAdmin,
+          },
+        });
     }
   } catch (err) {
     next(err);
