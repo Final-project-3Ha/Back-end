90abefeb803dd3a4f682053b471f89b7766e92a8
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-05-20 14:22:04 +0300

    finish OrderController

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -10,10 +10,16 @@ router.use(verifyIsLoggedIn);
 router.get("/", orderController.getUserOrders);
 router.get("/user/:id", orderController.getOrder);
 router.post("/", orderController.createOrder);
+router.put("/paid/:id", orderController.updateOrderToPaid);
 
 // Admin routes
 
 router.use(verifyIsAdmin)
+
+router.put("/delivered/:id", orderController.updateOrderToDelivered);
+router.get("/admin", orderController.getOrders);
+router.get("/analysis/:date", orderController.getOrderForAnalysis);
+
 
 
 export default router;
