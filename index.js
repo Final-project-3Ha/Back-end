3d167e3c7920641b0da9daecd890cca4aa1a9eaf
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-06-01 08:36:10 +0300

    *

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const app = new express();
 
 app.use(
   cors({
-    origin: "https://monuehshop.vercel.app",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: "*",
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204,
