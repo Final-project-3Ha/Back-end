8643a819fb53827bfd4e4eaba46908abdb7a6c63
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-06-01 05:05:04 +0300

    Edit API get-toke

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,15 +11,34 @@ app.get("/logout", (req, res) => {
   return res.clearCookie("access_token").send("access token cleared");
 });
 
+// app.get("/get-token", (req, res) => {
+//   try {
+//     const accessToken = req.cookies["access_token"];
+//     const decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
+//     return res.json({ token: decoded.name, isAdmin: decoded.isAdmin });
+//   } catch (err) {
+//     return res.status(401).send("Unauthorized. Invalid Token");
+//   }
+// });
+
 app.get("/get-token", (req, res) => {
   try {
     const accessToken = req.cookies["access_token"];
     const decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
+
+    // Set the SameSite attribute 
+    res.cookie("access_token", accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
+
     return res.json({ token: decoded.name, isAdmin: decoded.isAdmin });
   } catch (err) {
     return res.status(401).send("Unauthorized. Invalid Token");
   }
 });
+
 
 app.use('/products', productRoutes) 
 app.use('/categories', categoryRoutes)
